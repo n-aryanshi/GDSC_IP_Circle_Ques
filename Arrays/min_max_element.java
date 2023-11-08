390bffa4f1a49e9f960fec3c2502ad6f3934f6bc
Aryanshi Naglaksh <80567421+n-aryanshi@users.noreply.github.com>
2023-11-08 18:46:52 +0530

    Update min_max_element.java

--- Arrays/min_max_element.java
+++ Arrays/min_max_element.java
@@ -1,1 +1,23 @@
-
+class Solution
+{ 
+    public static int findSum(int A[],int N) 
+    {
+        //code here
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+    	for(int i=0; i<N;i++){
+    	    
+    	    if(A[i]>minValue){
+    	        minValue=A[i];
+    	    }
+    	    
+    	    if(A[i]<maxValue){
+    	        maxValue=A[i];
+    	    }
+    	    
+    	}
+    	
+    	int sum = minValue + maxValue;
+    	return sum;
+    }
+}

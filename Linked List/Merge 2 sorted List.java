39c1d4c1af6d2e5f6b239458bfbda80da9633628
Aryanshi Naglaksh <80567421+n-aryanshi@users.noreply.github.com>
2024-01-10 21:40:33 +0530

    Create Merge 2 sorted List.java

---
+++ Linked List/Merge 2 sorted List.java
@@ -0,0 +1,39 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode mergedLL=new ListNode(-1); //dummynode
+        ListNode temp=mergedLL; //traversing it with a temporary pointer
+
+        while(list1!=null && list2!=null){
+                if(list1.val<=list2.val){
+                    temp.next=list1;
+                    list1=list1.next;
+                    temp=temp.next;
+
+                }else{
+                    temp.next=list2;
+                    list2=list2.next;
+                    temp=temp.next;
+
+                }
+            }
+
+        while(list1!=null){
+            temp.next=list1;
+            list1=list1.next;
+            temp=temp.next;
+
+        }   
+
+        while(list2!=null){
+            temp.next=list2;
+            list2=list2.next;
+            temp=temp.next;
+
+        } 
+
+        return mergedLL.next;
+
+        }
+        
+    }
